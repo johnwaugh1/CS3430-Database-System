122582c04f66d7d4e3a5bca876caf879680fb2e1
John Waugh <122905252+johnwaugh1@users.noreply.github.com>
2024-04-16 13:43:18 -0400

    Create triggers.sql

---
+++ triggers.sql
@@ -0,0 +1,55 @@
+use cs_2024_spring_3430_101_t3;
+drop trigger if exists check_product_quantity_before_insert;
+drop trigger if exists check_product_quantity_before_update;
+drop trigger if exists update_inventory_after_order;
+drop trigger if exists check_inventory;
+
+-- check quantity before order placed
+delimiter //
+create trigger check_inventory
+before insert on orders
+for each row
+begin
+    declare product_quantity int;
+    select quantity into product_quantity from inventory where productID = new.productID;
+    if product_quantity < new.quanity then
+        signal sqlstate '45000' set message_text = 'Insufficient quantity in inventory';
+    end if;
+end//
+delimiter ;
+
+-- update the inventory after order
+delimiter //
+create trigger update_inventory_after_order
+after insert on orders
+for each row
+begin
+    update inventory
+    set quantity = quantity - new.quanity
+    where productID = new.productID;
+end//
+delimiter ;
+
+-- check product quantity before update
+delimiter //
+create trigger check_product_quantity_before_update
+before update on inventory
+for each row
+begin
+    if new.quantity < 0 then
+        signal sqlstate '45000' set message_text = 'Quantity cannot be negative';
+    end if;
+end//
+delimiter ;
+
+-- Check Product Quantity Before Insert
+delimiter //
+create trigger check_product_quantity_before_insert
+before insert on inventory
+for each row
+begin
+    if new.quantity < 0 then
+        signal sqlstate '45000' set message_text = 'Quantity cannot be negative';
+    end if;
+end//
+delimiter ;
